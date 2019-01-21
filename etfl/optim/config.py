bf8ac41a5fdc8e1ceeac74ebf9176e38a0184da0
Pierre Salvy <pierre.salvy@epfl.ch>
2019-01-21 09:29:36 +0100

    ENH: Adding Gurobi-tuned configs for gene KO and uptake vs growth studies

--- etfl/optim/config.py
+++ etfl/optim/config.py
@@ -11,6 +11,12 @@ Solver configuration helpers
 """
 
 def standard_solver_config(model, verbose = True):
+    """
+    Basic solver settings for ETFL
+    :param model:
+    :param verbose:
+    :return:
+    """
     # Solver settings
     model.solver.configuration.verbosity = int(verbose)
     model.solver.configuration.tolerances.feasibility = 1e-9
@@ -25,3 +31,40 @@ def standard_solver_config(model, verbose = True):
         model.solver.problem.Params.Method = -1  # Auto
         # model.solver.problem.Params.Method = 5  # deterministic concurrent simplex
         # model.solver.problem.Params.Aggregate = 0  # Turn off aggregation
+
+
+def gene_ko_config(model):
+    """
+    Solver settings for performing gene KO. Tuned using the grbtune tool on the
+    vETFL model iJO1366. The gene KO analysis is turned into a feasibility
+    problem by putting a lower bound on growth.
+
+    :param model:
+    :return:
+    """
+    standard_solver_config(model)
+    if model.solver.interface.__name__ == 'optlang.gurobi_interface':
+        # model.solver.problem.Params.MIPFocus = 1# Find a feasible solution
+        model.solver.problem.Params.Normadjust = 2#
+        model.solver.problem.Params.AggFill = 10#
+        model.solver.problem.Params.BranchDir = 1#
+        model.solver.problem.Params.PrePasses = 5#
+
+
+def growth_uptake_config(model):
+    """
+    Solver settings for performing growth vs uptake studies. Tuned using the
+    grbtune tool on the vETFL model iJO1366.
+     
+    :param model:
+    :return:
+    """
+    standard_solver_config(model)
+    if model.solver.interface.__name__ == 'optlang.gurobi_interface':
+        # model.solver.problem.Params.Cuts = 3 # Focus on finding feasible solutions
+        # model.solver.problem.Params.MIPFocus = 1 # Focus on finding feasible solutions
+        # model.solver.problem.Params.ConcurrentMIP = 4 # 4 independant solvers
+        model.solver.problem.Params.SimplexPricing = 2
+        model.solver.problem.Params.BranchDir = 1
+        model.solver.problem.Params.Heuristics = 0
+        model.solver.problem.Params.AggFill = 5
