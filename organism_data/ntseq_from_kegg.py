b1dff020813e8a4915c227059c194a62b0d68adf
Pierre Salvy <pierre.salvy@epfl.ch>
2019-03-28 16:59:46 +0100

    ENH: better E. coli data

--- organism_data/ntseq_from_kegg.py
+++ organism_data/ntseq_from_kegg.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import requests
 
+from os.path import join as pjoin
+
+data_folder = 'info_ecoli'
+
 kegg_url = "http://rest.kegg.jp/get/{org}:{gene_name}/ntseq"
 
 ecoli = cobra.io.load_json_model('../models/iJO1366.json')
@@ -11,11 +15,18 @@ all_b_genes = pd.Series(['eco:{}'.format(x.id) for x in ecoli.genes])
 
 rnap_genes = pd.Series(['eco:b3295','eco:b3649','eco:b3987','eco:b3988'])
 rrna_genes = pd.Series(['eco:b3851','eco:b3854','eco:b3855'])
-rpeptide_genes = pd.read_csv('ribosomal_proteins_ecoli.tsv',
+rpeptide_genes = pd.read_csv(pjoin(data_folder,'ribosomal_proteins_ecoli.tsv'),
                              delimiter='\t',
                              header=None)[0]
 
-all_b_genes = pd.concat([all_b_genes, rnap_genes, rrna_genes, rpeptide_genes])
+other_genes = 'eco:' + pd.read_csv(pjoin(data_folder,'coupling_dict_peptide_genes.csv'),
+                             index_col=0,
+                             header=None)
+other_genes.columns = [0]
+other_genes = other_genes[0]
+
+all_b_genes = pd.concat([all_b_genes, rnap_genes, rrna_genes, rpeptide_genes, other_genes])
+all_b_genes.drop_duplicates(inplace=True)
 
 def get_from_kegg(gene_id):
     org,gene_name = gene_id.split(':')
@@ -31,4 +42,4 @@ def get_from_kegg(gene_id):
 nt_sequences = all_b_genes.apply(get_from_kegg)
 nt_sequences.index = all_b_genes.str.split(':').apply(lambda x:x[1])
 nt_sequences.dropna(inplace = True)
-nt_sequences.to_csv('iJO1366_nt_seq_kegg.csv', )
+nt_sequences.to_csv(pjoin(data_folder,'iJO1366_nt_seq_kegg.csv'))
