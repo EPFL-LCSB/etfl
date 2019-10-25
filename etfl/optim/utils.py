af3012ab0512ece9aaf76d48e7733770fe254794
Pierre Salvy <pierre.salvy@epfl.ch>
2019-10-25 09:28:42 +0200

    MNT: Cleanup

--- etfl/optim/utils.py
+++ etfl/optim/utils.py
@@ -455,8 +455,10 @@ def safe_optim(model):
 
 def get_binding_constraints(model, epsilon):
 
-    if model.problem.__name__ == 'optlang.gurobi_interface':
+    if is_gurobi(model):
         return {kind:[c.name
                                for c in these_cons
                                if c.constraint._internal_constraint.Slack <= epsilon]
                 for kind,these_cons in model._cons_kinds.items()}
+    else:
+        raise(NotImplementedError)
