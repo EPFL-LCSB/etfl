cba6454f5089fc35ca55dd8f9f452b4b76a8256d
Omid <omid.oftadeh@epfl.ch>
2021-03-01 11:22:00 +0100

    DOC: updating the setup file

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ version_tag = '0.0.2'
 
 setup(name='ETFL',
       version=version_tag,
-      author='Pierre SALVY',
+      author='Pierre SALVY, Omid OFTADEH',
       author_email='softwares.lcsb@epfl.ch',
       url='https://github.com/EPFL-LCSB/etfl/',
       download_url='https://github.com/EPFL-LCSB/etfl/archive/'+version_tag+'.tar.gz',
